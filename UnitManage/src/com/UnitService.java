0e35aaefeae8d52d6a536e8c2f79ab5b72aa3ad4
Tharushi Hewage <it20251932@my.sliit.lk>
2022-04-25 22:49:24 +0530

    Creating PUT request for monthly unit management service

--- UnitManage/src/com/UnitService.java
+++ UnitManage/src/com/UnitService.java
@@ -38,4 +38,25 @@ public class UnitService {
 	 String output = UnitObj.insertUnit (uAccNo, uDate, UnitAmount, PriceForPerUnit, Total);
 	return output;
 	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateUnit(String unitData)
+	{
+		
+	//Converting the string input to a JSON object
+	 JsonObject unitObject = new JsonParser().parse(unitData).getAsJsonObject();
+	 
+	//Reading the values from the JSON object
+	 String uID = unitObject.get("uID").getAsString();
+	 String uAccNo = unitObject.get("uAccNo").getAsString();
+	 String uDate = unitObject.get("uDate").getAsString();
+	 String UnitAmount = unitObject.get("UnitAmount").getAsString();
+	 String PriceForPerUnit = unitObject.get("PriceForPerUnit").getAsString();
+	 String Total = unitObject.get("Total").getAsString();
+	 String output = UnitObj.updateUnit(uID, uAccNo, uDate, UnitAmount, PriceForPerUnit, Total);
+	return output;
+	} 
 }
